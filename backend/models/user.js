dc1c99c34fc1858d9d44ad7cf02d1f34229b6d97
Cassius <cassius.reynolds.dev@gmail.com>
2024-08-01 14:27:46 -0700

    configured token to return after creating user and omitted the password field from the token

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,32 +1,3 @@
-("use strict");
-const { Model } = require("sequelize");
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      User.hasMany(models.Exhibition, { foreignKey: "userId" });
-    }
-  }
-  User.init(
-    {
-      username: DataTypes.STRING,
-      password: DataTypes.STRING,
-      profileImg: DataTypes.STRING,
-    },
-    {
-      sequelize,
-      modelName: "User",
-    }
-  );
-  return User;
-};
-
-// models/User.js
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     id: {
@@ -47,6 +18,11 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
     },
   });
-
+  // Add the toJSON method to exclude the password field
+  User.prototype.toJSON = function () {
+    const values = Object.assign({}, this.get());
+    delete values.password;
+    return values;
+  };
   return User;
 };
--- backend/routes/test-jwt.js
+++ backend/routes/test-jwt.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const tokenRouter = require('../controllers/test-jwt')
 
-router.get('/sign-token', tokenRouter.sign)
+
 router.post('/verify-token', tokenRouter.verify)
 router.post('/refresh-token', tokenRouter.refresh)
 
