8b3a820ceb68ee83add07e7cd7f1f6030b36208f
molliean <molliean29@gmail.com>
2024-08-04 11:36:53 -0700

    fixed lamp component on homepage and added it to the landing page

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,24 +1,19 @@
-import {Link} from 'react-router-dom'
-import { getAllArtworks } from '../../services/artworkService';
+import { Link } from 'react-router-dom'
+// import { getAllArtworks } from '../../services/artworkService';
 import { LampContainer } from '../ui/lamp';
 
 const Home = () => {
-  getAllArtworks()
+  // getAllArtworks()
   return (
-    <>
-    <section>
-    <LampContainer>
-      <div className="min-h-screen w-screen flex flex-col gap-y-5 justify-center items-center">
-        <h1 className="text-8xl">Welcome, user</h1>
-        <Link to="/exhibitions/dashboard">
-        <button className="text-4xl border-2 border-black p-5 m-6">My Exhibitions</button>
 
-        </Link>
-      </div>
+      <LampContainer>
+        <div className="relative z-50 min-h-screen w-screen flex flex-col gap-y-5 justify-center items-center">
+          <h1 className="text-8xl text-white">Welcome, user</h1>
+          <Link to="/exhibitions/dashboard">
+            <button className="relative text-4xl border-2 text-white p-5 m-6">My Exhibitions</button>
+          </Link>
+        </div>
       </LampContainer>
-    </section>
-    </>
-
   );
 };
 export default Home;
