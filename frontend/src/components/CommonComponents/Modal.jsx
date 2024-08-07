d2514fb71708ab49439d872895f53da29a2e49ef
molliean <molliean29@gmail.com>
2024-08-07 16:12:59 -0700

    resolved modal change

--- frontend/src/components/CommonComponents/Modal.jsx
+++ frontend/src/components/CommonComponents/Modal.jsx
@@ -1,55 +1,59 @@
 import { useEffect, useState } from "react";
 import { postAddArtworkToExb } from "../../services/exbService";
+import useExbContext from "../../context/exb/useExbContext";
 
 const Modal = ({ isVisible, onClose, exbs, ArtworkObjectid }) => {
-  const [message, setMessage] = useState("");
-  useEffect(() => {
-    // console.log(ArtworkObjectid, ' art obj id')
-  }, []);
-  if (!isVisible) {
-    return null;
-  }
-
-  const handleAddArtworkToExb = async (exbId, objectid) => {
-    try {
-      const data = await postAddArtworkToExb(exbId, objectid);
-      console.log(data);
-      if (data.message) {
-        setMessage(data.message);
-      }
-    } catch (err) {
-      console.error(err);
-      console.log(`Cannot communicate with DB to add artwork to exb`);
+    const [message, setMessage] = useState("");
+    const { myExbs } = useExbContext();
+    useEffect(() => {
+        // console.log(ArtworkObjectid, ' art obj id')
+        console.log(myExbs)
+    }, []);
+    if (!isVisible) {
+        return null;
     }
-  };
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-start justify-center z-50 modal">
-      <div className="flex flex-col items-center justify-center gap-4 bg-white p-8 h-96 rounded-lg shadow-lg w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <button
-          className="absolute top-0 right-3 text-4xl modal-close"
-          onClick={() => {
-            onClose();
-            setMessage("");
-          }}
-        >
-          &times;
-        </button>{" "}
-        <p>Click Exhibition to Add Artwork</p>
-        {message && <p>{message}</p>}
-        <ul className="bg-neutral-100 w-full md:w-1/2 mb-4 h-80 overflow-y-scroll ">
-          {exbs?.map((exb, idx) => (
-            <li
-              onClick={() => handleAddArtworkToExb(exb.id, ArtworkObjectid)}
-              key={idx}
-              className="p-3 hover:bg-neutral-200 cursor-pointer"
-            >
-              {exb.title}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+
+
+    const handleAddArtworkToExb = async (exbId, objectid) => {
+        try {
+            const data = await postAddArtworkToExb(exbId, objectid);
+            console.log(data);
+            if (data.message) {
+                setMessage(data.message);
+            }
+        } catch (err) {
+            console.error(err);
+            console.log(`Cannot communicate with DB to add artwork to exb`);
+        }
+    };
+    return (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-start justify-center z-50 modal">
+            <div className="flex flex-col items-center justify-center gap-4 bg-white p-8 h-96 rounded-lg shadow-lg w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <button
+                    className="absolute top-0 right-3 text-4xl modal-close"
+                    onClick={() => {
+                        onClose();
+                        setMessage("");
+                    }}
+                >
+                    &times;
+                </button>{" "}
+                <p>Click Exhibition to Add Artwork</p>
+                {message && <p>{message}</p>}
+                <ul className="bg-neutral-100 w-full md:w-1/2 mb-4 h-80 overflow-y-scroll ">
+                    {exbs?.map((exb, idx) => (
+                        <li
+                            onClick={() => handleAddArtworkToExb(exb.id, ArtworkObjectid)}
+                            key={idx}
+                            className="p-3 hover:bg-neutral-200 cursor-pointer"
+                        >
+                            {exb.title}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default Modal;
