ee673431a33882b79b226b202d88890a0f17449a
Cassius <cassius.reynolds.dev@gmail.com>
2024-08-11 21:19:03 -0700

    fixed logic for finding and replacing the place holder or api key to use a more consistent approach rather than trying to assume the string length with slice. Now one function uses a regex match method to look for the desired text based upon the newValue we want to replace it with

--- backend/controllers/artwork.js
+++ backend/controllers/artwork.js
@@ -1,4 +1,3 @@
-
 // env vars
 const BASE_URL = process.env.HARVARD_API_BASE_URL;
 const API_KEY = process.env.API_KEY;
@@ -19,7 +18,10 @@ const getArtworks = async (req, res) => {
       `${BASE_URL}/object?apikey=${API_KEY}${queriedFilter}`
     );
     let data = await response.json();
-    data.info.next = replaceApikeyWithPlaceholder(data.info.next);
+
+    data.info.next = swapApiKeyAndPlaceholder(data.info.next, "API_KEY");
+    console.log(data.info.next, " <-- next");
+
     data.info.prev = "";
     res.status(200).json(data);
   } catch (err) {
@@ -60,7 +62,7 @@ const getFilterObjs = async (req, res) => {
       `${BASE_URL}/${filter}?apikey=${API_KEY}&size=100&page=${page}`
     );
     let data = await response.json();
-    data.info.next = replaceApikeyWithPlaceholder(data.info.next);
+    data.info.next = swapApiKeyAndPlaceholder(data.info.next, "API_KEY");
     data.info.prev = "";
     res.status(200).json(data);
   } catch (err) {
@@ -78,8 +80,8 @@ const getArtworkBySearch = async (req, res) => {
   const { searchQuery } = req.query;
 
   // remove searchQuery key to add the rest of the queries to the end
-let queryObjWithoutSearchQuery = req.query;
-delete queryObjWithoutSearchQuery.searchQuery
+  let queryObjWithoutSearchQuery = req.query;
+  delete queryObjWithoutSearchQuery.searchQuery;
 
   const queriedFilter = getQueryString(queryObjWithoutSearchQuery);
   try {
@@ -87,7 +89,7 @@ delete queryObjWithoutSearchQuery.searchQuery
       `${BASE_URL}/object?q=${searchQuery}&apikey=${API_KEY}${queriedFilter}`
     );
     let data = await response.json();
-    data.info.next = replaceApikeyWithPlaceholder(data.info.next);
+    data.info.next = swapApiKeyAndPlaceholder(data.info.next, "API_KEY");
     data.info.prev = "";
     res.status(200).json(data);
   } catch (err) {
@@ -105,14 +107,13 @@ const postNextPageOfArtworks = async (req, res) => {
   const { altUrl } = req.body;
   // the frontend removes the api key and replaces it with a placeholder | API_KEY
   // we select the string before and after the place holder, then insert our key in place of it
-  const beforeApiKey = altUrl.slice(0, 48);
-  const afterApiKey = altUrl.slice(55);
-  const fullUrl = beforeApiKey + API_KEY + afterApiKey;
+
+  const fullUrl = swapApiKeyAndPlaceholder(altUrl, API_KEY);
+
   try {
     const response = await fetch(fullUrl);
-
     let data = await response.json();
-    data.info.next = replaceApikeyWithPlaceholder(data.info.next);
+    data.info.next = swapApiKeyAndPlaceholder(data.info.next, "API_KEY");
     data.info.prev = "";
     res.status(200).json(data);
   } catch (err) {
@@ -144,14 +145,23 @@ const getQueryString = (query) => {
 };
 
 // replace api key with place holder
-const replaceApikeyWithPlaceholder = (str) => {
-  const apiKeyPattern = /(apikey=)([a-f0-9-]+)(&)/;
+const swapApiKeyAndPlaceholder = (str, newValue, log) => {
+  let apiKeyPattern;
+  // if the value we want to put in is the real api key
+  if (newValue !== "API_KEY") {
+    apiKeyPattern = /(apikey=)(API_KEY)(&)/;
+  } else {
+    // if we want to add a placeholder
+    apiKeyPattern = /(apikey=)([a-f0-9-]+)(&)/;
+  }
   // Use match to extract the parts
   const matches = str?.match(apiKeyPattern);
   if (matches) {
     const beforeApiKey = str.substring(0, matches.index + matches[1].length); // ex: "https://api.harvardartmuseums.org/object?apikey="
     const afterApiKey = str.substring(matches.index + matches[0].length - 1); // ex: "&size=12&page=2"
-    str = beforeApiKey + "API_KEY" + afterApiKey;
+
+    str = beforeApiKey + newValue + afterApiKey;
+
     return str;
   }
 };
