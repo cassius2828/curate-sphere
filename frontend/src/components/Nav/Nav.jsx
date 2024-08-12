face7d05820ba96ae2e11cc7d84f75386c7dc15e
Cassius <cassius.reynolds.dev@gmail.com>
2024-08-11 22:09:29 -0700

    login dropdown now disappears on mobile after clicking the desired action

--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -34,11 +34,11 @@ const Nav = () => {
           <ul className="absolute right-32 mt-2 bg-[#020617] text-gray-100 font-marcellus flex flex-col items-start p-4 space-y-4 rounded-lg shadow-lg">
             {!user ? (
               <>
-                <Link to={`/login`}>
+                <Link onClick={() => setIsMenuOpen(false)} to={`/login`}>
                   <li className="p-3 text-2xl ">Login</li>
                 </Link>
 
-                <Link to={`/register`}>
+                <Link onClick={() => setIsMenuOpen(false)}  to={`/register`}>
                   <li className="p-3 text-2xl ">Register</li>
                 </Link>
               </>
@@ -93,11 +93,11 @@ const Nav = () => {
       <ul className="hidden md:flex justify-end gap-12 capitalize w-full md:w-1/2 items-center">
         {!user ? (
           <>
-            <Link to={`/login`}>
+            <Link onClick={() => setIsMenuOpen(false)}  to={`/login`}>
               <li className="p-3 text-2xl ">Login</li>
             </Link>
 
-            <Link to={`/register`}>
+            <Link onClick={() => setIsMenuOpen(false)}  to={`/register`}>
               <li className="p-3 text-2xl ">Register</li>
             </Link>
           </>
