31d3a09cbcd98d344aaf4a0b1c552052016e501a
Cassius <cassius.reynolds.dev@gmail.com>
2024-08-13 21:37:46 -0700

    added ability to search your exbs when adding an artwork to it

--- frontend/src/components/CommonComponents/Modal.jsx
+++ frontend/src/components/CommonComponents/Modal.jsx
@@ -1,13 +1,38 @@
 import { useState } from "react";
 import { postAddArtworkToExb } from "../../services/exbService";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-const Modal = ({ isVisible, onClose, exbs=[], ArtworkObjectid }) => {
+const Modal = ({ isVisible, onClose, exbs = [], ArtworkObjectid }) => {
   const [message, setMessage] = useState("");
+  const [displayUserExbs, setDisplayUserExbs] = useState(exbs);
   if (!isVisible) {
     return null;
   }
+  ///////////////////////////
+  // Search Functions / Actions
+  ///////////////////////////
+  const handleSearchQuery = (e) => {
+    const { value } = e.target;
+    filterdisplayUserExbsBySearch(value);
+  };
 
+  const filterdisplayUserExbsBySearch = (searchQuery) => {
+    // begins search when query is at least 3 chars long
+    if (searchQuery.length > 2)
+      setDisplayUserExbs(
+        displayUserExbs.filter((exb) =>
+          exb.title.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      );
+    // resets results if user cleared search
+    if (searchQuery.length === 0) setDisplayUserExbs(exbs);
+  };
+
+  ///////////////////////////
+  // Add artwork to exb
+  ///////////////////////////
   const handleAddArtworkToExb = async (exbId, objectid) => {
     try {
       const data = await postAddArtworkToExb(exbId, objectid);
@@ -46,8 +71,21 @@ const Modal = ({ isVisible, onClose, exbs=[], ArtworkObjectid }) => {
         ) : (
           <p className="text-red-500">{message}</p>
         )}
+        <div className="relative flex flex-col justify-start">
+          {/* search input */}
+          <input
+            // value={query}
+            onChange={handleSearchQuery}
+            className=" border-4 border-neutral-900 p-2 mt-4 mb-6 w-full "
+            type="text"
+          />
+          <FontAwesomeIcon
+            className="absolute top-8 right-5  text-2xl "
+            icon={faSearch}
+          />
+        </div>
         <ul className="bg-neutral-100 w-full md:w-1/2 mb-4 h-80 overflow-y-scroll ">
-          {exbs?.map((exb, idx) => (
+          {displayUserExbs?.map((exb, idx) => (
             <li
               onClick={() => handleAddArtworkToExb(exb.id, ArtworkObjectid)}
               key={idx}
