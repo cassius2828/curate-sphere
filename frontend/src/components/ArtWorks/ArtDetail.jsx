1bc27bb02a6f1d8585d7ebb55f1f5dacf9bf5ca9
Cassius <cassius.reynolds.dev@gmail.com>
2024-08-26 22:47:35 -0700

    process of restyling the artwork details page to reflect more similarities and functionality of the harvard collections page

--- frontend/src/components/ArtWorks/ArtDetail.jsx
+++ frontend/src/components/ArtWorks/ArtDetail.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { getArtworkDetail } from "../../services/artworkService";
 import Modal from "../CommonComponents/Modal";
 import useExbContext from "../../context/exb/useExbContext";
@@ -35,6 +35,7 @@ const ArtDetail = () => {
     title,
     medium,
     dimensions,
+    classification,
   } = artDetails;
   ///////////////////////////
   // Modal Actions
@@ -64,7 +65,7 @@ const ArtDetail = () => {
       if (data.token) {
         localStorage.setItem("token", data.token);
         setUser(getUser());
-        console.log(user, )
+        console.log(user);
       }
     } catch (err) {
       console.error(err);
@@ -99,17 +100,34 @@ const ArtDetail = () => {
   };
   useEffect(() => {
     fetchArtworkDetails();
+    console.log(artDetails);
   }, []);
 
   if (isLoading) return <Loader />;
   return (
     <section className="p-4">
-      {/* title */}
-      <h1 className="text-5xl md:text-6xl mb-12 md:mb-24 text-center font-marcellus">
-        {title}
-      </h1>
+      {/* header */}
+      <header className="shadow-md pb-1 flex items-center justify-between w-full">
+        <span className="text-5xl font-marcellus ml-5">CS/</span>
+        <div className="flex flex-col justify-end items-center gap-6 mb-6">
+          <h1 className="text-3xl md:text-4xl  text-center font-marcellus">
+            <span className="text-gray-500">{id}:</span> {title}
+          </h1>
+          <span className="text-xl font-bold text-gray-500">
+            {classification}
+          </span>
+        </div>
+        <div>
+          <span
+            onClick={() => navigate(-1)}
+            className="text-2xl hover:border-gray-600 border-transparent border p-2 mr-5 transition-all duration-200 cursor-pointer"
+          >
+            &larr; back
+          </span>
+        </div>
+      </header>
       {/* img */}
-      <div className="flex flex-col md:flex-row justify-center items-center gap-10 my-20">
+      <div className="flex flex-col  justify-center items-center gap-10 my-20">
         {isLoadingImg ? (
           <div className="w-full md:w-1/2 md:h-full flex flex-col items-center gap-8">
             <LoaderRipple />
@@ -126,52 +144,78 @@ const ArtDetail = () => {
             alt={title}
           />
         )}
+
         {/* 
        
      
        */}
-        <div className="flex flex-col justify-center mx-10 items-start gap-6 md:gap-20 md:items-start md:mx-2 text-3xl font-cardo">
-          {/* artists */}
-          {message && (
-            <span className="text-green-500 text-3xl mx-auto">{message}</span>
-          )}
-          <ul>
-            {people?.map((person) => (
-              <li className=" my-4" key={person.personid}>
-                {person.role}: {person.name}
-              </li>
-            ))}
-          </ul>
-          {/* other info */}
-          <p>Date: {dated ? dated : "N/A"}</p>
-          <p>Medium: {medium ? medium : "N/A"}</p>
-          <p>Dimensions: {dimensions ? dimensions : "N/A"}</p>
-          <p>Division: {division ? division : "N/A"}</p>
+        <div></div>
+      </div>
+      {/* btns */}
+      <div className="w-1/2 max-w-96  mx-auto  ">
+        <ul className="flex items-center justify-around gap-3 ">
+          {/* add to exb */}
+          <li className="p-3 border border-gray-500">X</li>
+          {/* make profile pic */}
+          <li className="p-3 border border-gray-500">X</li>
+          {/* make header */}
+          <li className="p-3 border border-gray-500">X</li>
+          {/* copy url */}
+          <li className="p-3 border border-gray-500">X</li>
+          {/* citation */}
+          <li className="p-3 border border-gray-500">X</li>
+        </ul>
+      </div>
+      {/* details */}
+      <div className="flex flex-col justify-center mx-10 mt-12 items-start gap-6 md:gap-20 md:items-start md:mx-2 text-3xl font-cardo border-t-2">
+        {/* artists */}
+        {message && (
+          <span className="text-green-500 text-3xl mx-auto">{message}</span>
+        )}
+        <ul>
+          {people?.map((person) => (
+            <li className=" my-4" key={person.personid}>
+              {person.role}: {person.name}
+            </li>
+          ))}
+        </ul>
+        {/* other info */}
+        <p>Date: {dated ? dated : "N/A"}</p>
+        <p>Medium: {medium ? medium : "N/A"}</p>
+        <p>Dimensions: {dimensions ? dimensions : "N/A"}</p>
+        <p>Division: {division ? division : "N/A"}</p>
 
-          {/* action btn */}
-          <div className="flex items-center justify-center w-full md:w-3/4 mx-auto gap-12">
+        {/* action btn */}
+        <div className="flex flex-col items-center justify-center w-full md:w-3/4 mx-auto gap-12">
+          {user && (
+            <div className="gap-12 w-full flex items-center">
+              <button
+                onClick={() =>
+                  handleUpdateUserImgByArtworkUrl(primaryimageurl, "profile")
+                }
+                className="border border-black px-6 py-1 font-cardo md:w-1/2 mt-6 md:mt-auto mx-auto"
+              >
+                Make Profile Picture
+              </button>
+              <button
+                onClick={() =>
+                  handleUpdateUserImgByArtworkUrl(primaryimageurl, "header")
+                }
+                className="border border-black px-6 py-1 font-cardo md:w-1/2 mt-6 md:mt-auto mx-auto"
+              >
+                Make Profile Header
+              </button>
+            </div>
+          )}
+
+          <div className="gap-12 w-full flex items-center">
             <button
               onClick={() => navigate(-1)}
               className="border border-black px-6 py-1 font-cardo md:w-1/2 mt-6 md:mt-auto mx-auto"
             >
               Back
             </button>
-            <button
-              onClick={() =>
-                handleUpdateUserImgByArtworkUrl(primaryimageurl, "profile")
-              }
-              className="border border-black px-6 py-1 font-cardo md:w-1/2 mt-6 md:mt-auto mx-auto"
-            >
-              Make Profile Picture
-            </button>
-            <button
-              onClick={() =>
-                handleUpdateUserImgByArtworkUrl(primaryimageurl, "header")
-              }
-              className="border border-black px-6 py-1 font-cardo md:w-1/2 mt-6 md:mt-auto mx-auto"
-            >
-              Make Profile Header
-            </button>
+
             <button
               onClick={showModal}
               className="border border-black px-6 py-1 font-cardo md:w-1/2 mt-6 md:mt-auto mx-auto"
