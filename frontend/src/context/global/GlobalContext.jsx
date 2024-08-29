68abcb0834ddb472d9e0da1a1849d2054f98a6c6
Cassius <cassius.reynolds.dev@gmail.com>
2024-08-28 21:13:36 -0700

    fixed an infinite loop caused by added dependencies to a useEffect in exbDetail that was uncaught from formatting and optimizing code. Also fixed date picker issue where the selected date was not in sync with the displayed date. This was solved by ensuring we were not unnecessarily changing the date to a js date object when it would be fine to be just a string. UTC timezone of the date object was not in sync with PST

--- frontend/src/context/global/GlobalContext.jsx
+++ frontend/src/context/global/GlobalContext.jsx
@@ -32,11 +32,8 @@ export const GlobalProvider = ({ children }) => {
     });
   };
   const formatDateForEdit = (dateString) => {
-    const date = new Date(dateString);
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-    return `${year}-${month}-${day}`;
+    if (!dateString) return ""; 
+    return dateString.split("T")[0]; 
   };
 
   ///////////////////////////
